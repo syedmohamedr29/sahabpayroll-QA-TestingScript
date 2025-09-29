1a29d169b5c89d0c97bd7e52458b23af2f57c72a
syedmohamedr29 <syedmohamedr29@gmail.com>
2025-09-29 17:28:42 +0530

    AUtomation - Loan POST API

--- API_Automation/apis/loanAPI/loanAPIs.py
+++ API_Automation/apis/loanAPI/loanAPIs.py
@@ -1,7 +1,21 @@
+import json
+
 class LoanAPI:
-    ENDPOINT = "/api/CompBenefits/companies/{company_id}/loans"
+    LandingPage_ENDPOINT = "/api/CompBenefits/companies/{company_id}/loans"
+    PostAPI_ENDPOINT = "/api/CompBenefits/companies/{companyId}/loans"
 
     @staticmethod
-    def get_loans(request_context, company_id: str):
-        response = request_context.get(LoanAPI.ENDPOINT.format(company_id=company_id))
+    def get_loans_LandingPageAPI(request_context, company_id: str):
+        response = request_context.get(LoanAPI.LandingPage_ENDPOINT.format(company_id=company_id))
+        return response
+
+    @staticmethod
+    def loan_POSTAPI(request_context, company_id: str, payload: dict):
+        """
+        Sends a POST request to create a loan for a company.
+        """
+        response = request_context.post(
+            LoanAPI.PostAPI_ENDPOINT.format(companyId=company_id),
+            data=json.dumps(payload)
+        )
         return response
--- API_Automation/tests/LoanTest/loanTest.py
+++ API_Automation/tests/LoanTest/loanTest.py
@@ -1,6 +1,7 @@
 import pytest
 from API_Automation.utils.api_client import create_request_context
 from API_Automation.apis.loanAPI.loanAPIs import LoanAPI
+import json
 
 @pytest.mark.parametrize("company_id", [
     "3a1bfc8b-d3a6-8fad-a1e3-cc095d479d38"
@@ -10,16 +11,49 @@ def test_get_loans(playwright, company_id):
     request_context = create_request_context(playwright)
 
     # Call Loan API
-    response = LoanAPI.get_loans(request_context, company_id)
-
+    response = LoanAPI.get_loans_LandingPageAPI(request_context, company_id)
     # Assertions
+    assert response.status == 200, f"API failed: {response.text()}"
     assert response.ok, f"Request failed with status {response.status}"
+    """
+    {
+      "summary": {
+        "totalPausedLoans": 0,
+        "totalPauseScheduledLoans": 0,
+        "totalCreatedPendingProcessing": 0,
+        "totalCreatedPendingDisbursement": 0
+      },
+      "count": 0,
+      "output": []
+    }
+    """
+    json_data = response.json()
+    print("Response JSON:", json_data)
+    print(json_data["summary"]["totalPausedLoans"])
+    print("Count:", json_data["count"])
+
+
+
+@pytest.mark.parametrize("company_id", [
+    "3a1bfc8b-d3a6-8fad-a1e3-cc095d479d38"
+])
+def test_post_Loan(playwright, company_id):
+    request_context = create_request_context(playwright)
+
+    with open(
+        r"C:/Users/Comm-IT India/OneDrive - comm-it india pvt ltd/Desktop/SahabPayroll/API_Automation/data/loan/postJSON.json",
+        "r"
+    ) as f:
+        jsonPayload = json.load(f)
+
+
+    response = LoanAPI.loan_POSTAPI(request_context, company_id, jsonPayload)
+
+    assert response.status == 200, f"API failed: {response.text()}"
+    assert response.ok, f"Request failed with status {response.status}"
+
     json_data = response.json()
     print("Response JSON:", json_data)
 
-    """
-    # Extract employee counts if present
-    if isinstance(json_data, list):
-        employees = [item.get("totalNoOfEmployees") for item in json_data if "totalNoOfEmployees" in item]
-        print("Employee Counts:", employees)
-        """
+
+
